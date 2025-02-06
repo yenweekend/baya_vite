f3f7f205bc4a7a7d803c30ca5d5afc65fba37c3d
NGUYEN VAN YEN <nguyenvanyen25022004@gmail.com>
2025-02-06 23:39:09 +0700

    remove slice file

--- src/redux-toolkit/store/store.jsx
+++ src/redux-toolkit/store/store.jsx
@@ -1,11 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import brandReducer from "../slice/brandSlice";
-import homeReducer from "../slice/homeSlice";
+
 import variantSlice from "../slice/variantSlice";
 const store = configureStore({
   reducer: {
-    brand: brandReducer,
-    homepage: homeReducer,
     variant: variantSlice,
   },
 });
